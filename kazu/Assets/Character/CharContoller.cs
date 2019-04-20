d6e0e457f98f713a568813c94e1280741a6f2ae8
kazu <gambal.job@gmail.com>
2019-04-20 18:53:59 +0900

    gitignoreの追加
    キャラクターのデータ管理、時間が来たら描画をオフにする

---
+++ kazu/Assets/Character/CharContoller.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharContoller : MonoBehaviour
+{
+    //キャラの種類
+    enum CharValue
+    {
+        CAT = 1,
+        MAX_CHAR
+    }
+    //キャラのデータ
+    //キャラ１
+    float[] time = new float[(int)CharValue.MAX_CHAR];
+    Vector3[] scale = new Vector3[(int)CharValue.MAX_CHAR];
+
+    [SerializeField]
+    GameObject[] _charPre = new GameObject[(int)CharValue.MAX_CHAR];    //キャラのプレハブ
+    GameObject[] _char;                                                 //インスタンス用のオブジェ
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //データの保存
+        _char = new GameObject[(int)CharValue.MAX_CHAR];
+
+        scale[(int)CharValue.CAT] = new Vector3(1, 1, 1);
+        time[(int)CharValue.CAT] = 5.0f;
+
+        //インスタンス化
+        _char[(int)CharValue.CAT] = Instantiate(_charPre[(int)CharValue.CAT], new Vector3(0, 0, 0), Quaternion.identity);
+        _char[(int)CharValue.CAT].SetActive(true);
+
+        //データの格納
+        CharData temp = _char[(int)CharValue.CAT].GetComponent<CharData>();
+        temp.SetScale(scale[(int)CharValue.CAT]);
+        temp.SetTime(time[(int)CharValue.CAT]);
+
+        Debug.Log("時間" + _char[(int)CharValue.CAT].GetComponent<CharData>().GetTime());
+        Debug.Log("スケール" + _char[(int)CharValue.CAT].GetComponent<CharData>().GetScale());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
---
+++ kazu/Assets/Character/CharData.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharData : MonoBehaviour
+{
+    //メンバ変数
+    float _charTime;       //出現時間
+    bool _tFlag = false;   //カウントを始めるか
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //一定時間を過ぎたら出現をやめる
+        if (_tFlag)
+        {
+            _charTime -= Time.deltaTime;
+        }
+
+        if (_charTime <= 0)
+        {
+            _tFlag = false;
+            gameObject.SetActive(false);
+        }
+    }
+
+    //ゲームオブジェクトの変換
+    public GameObject GetGameObject()
+    {
+        return gameObject;
+    }
+
+    //出現時間をセット
+    public void SetTime(float time)
+    {
+        _charTime = time;
+        _tFlag = true;
+    }
+
+    //出現時間のゲット
+    public int GetTime()
+    {
+        return (int)_charTime;
+    }
+
+    //Scaleの設定
+    public void SetScale(Vector3 scale)
+    {
+        gameObject.transform.localScale = scale;
+    }
+
+    //Scaleのゲット 
+    public Transform GetScale()
+    {
+        return gameObject.transform;
+    }
+
+    public Collider2D GetRd()
+    {
+        return gameObject.GetComponent<Collider2D>();
+    }
+}
+
