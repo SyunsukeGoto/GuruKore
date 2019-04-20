e44c74a0ffe7cb7d1daf8652cf223a86e3be661d
SyunsukeGoto <gtoshun@gmail.com>
2019-04-20 16:04:16 +0900

    プロジェクト追加

---
+++ グルコレ/Assets/Scenes/Goto/Coordinate/LonLatGetter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/
+/// <file>		LonLatGetter.cs
+/// 
+/// <date>		2019/4/20
+/// 
+/// <author>	後藤　駿介
+//__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/
+
+
+///-----------------------------------------------------------------------------
+/// 
+/// </brief> 座標関連クラス
+///
+///-----------------------------------------------------------------------------
+
+namespace Goto
+{
+    public class LonLatGetter : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
