0b2983027b6f8d9e2b718c8ae17cd2daf39b96cf
kazu <gambal.job@gmail.com>
2019-04-21 10:34:05 +0900

    キャラクタースプライトの追加、三体のキャラを打ち込んでポジションを変える

--- CharacterSystem/Assets/Character/CharContoller.cs
+++ CharacterSystem/Assets/Character/CharContoller.cs
@@ -7,7 +7,9 @@ public class CharContoller : MonoBehaviour
     //キャラの種類
     enum CharValue
     {
-        CAT = 1,
+        CAT1,
+        CAT2,
+        CAT3,
         MAX_CHAR
     }
     //キャラのデータ
@@ -26,20 +28,40 @@ public class CharContoller : MonoBehaviour
         //データの保存
         _char = new GameObject[(int)CharValue.MAX_CHAR];
 
-        scale[(int)CharValue.CAT] = new Vector3(1, 1, 1);
-        time[(int)CharValue.CAT] = 5.0f;
+        scale[(int)CharValue.CAT1] = new Vector3(1, 1, 1);
+        time[(int)CharValue.CAT1] = 5.0f;
+
+        scale[(int)CharValue.CAT2] = new Vector3(1, 1, 1);
+        time[(int)CharValue.CAT2] = 5.0f;
+
+        scale[(int)CharValue.CAT3] = new Vector3(2, 2, 1);
+        time[(int)CharValue.CAT3] = 5.0f;
 
         //インスタンス化
-        _char[(int)CharValue.CAT] = Instantiate(_charPre[(int)CharValue.CAT], new Vector3(0, 0, 0), Quaternion.identity);
-        _char[(int)CharValue.CAT].SetActive(true);
+        _char[(int)CharValue.CAT1] = Instantiate(_charPre[(int)CharValue.CAT1], new Vector3(0, 0, 0), Quaternion.identity);
+        _char[(int)CharValue.CAT1].SetActive(true);
+
+        _char[(int)CharValue.CAT2] = Instantiate(_charPre[(int)CharValue.CAT2], new Vector3(3, 3, 3), Quaternion.identity);
+        _char[(int)CharValue.CAT2].SetActive(true);
+
+        _char[(int)CharValue.CAT3] = Instantiate(_charPre[(int)CharValue.CAT3], new Vector3(-3, -3, -3), Quaternion.identity);
+        _char[(int)CharValue.CAT3].SetActive(true);
 
         //データの格納
-        CharData temp = _char[(int)CharValue.CAT].GetComponent<CharData>();
-        temp.SetScale(scale[(int)CharValue.CAT]);
-        temp.SetTime(time[(int)CharValue.CAT]);
+        CharData cat1 = _char[(int)CharValue.CAT1].GetComponent<CharData>();
+        cat1.SetScale(scale[(int)CharValue.CAT1]);
+        cat1.SetTime(time[(int)CharValue.CAT1]);
 
-        Debug.Log("時間" + _char[(int)CharValue.CAT].GetComponent<CharData>().GetTime());
-        Debug.Log("スケール" + _char[(int)CharValue.CAT].GetComponent<CharData>().GetScale());
+        CharData cat2 = _char[(int)CharValue.CAT2].GetComponent<CharData>();
+        cat2.SetScale(scale[(int)CharValue.CAT2]);
+        cat2.SetTime(time[(int)CharValue.CAT2]);
+
+        CharData cat3 = _char[(int)CharValue.CAT3].GetComponent<CharData>();
+        cat3.SetScale(scale[(int)CharValue.CAT3]);
+        cat3.SetTime(time[(int)CharValue.CAT3]);
+
+        Debug.Log("時間" + _char[(int)CharValue.CAT1].GetComponent<CharData>().GetTime());
+        Debug.Log("スケール" + _char[(int)CharValue.CAT1].GetComponent<CharData>().GetScale());
     }
 
     // Update is called once per frame
---
+++ CharacterSystem/Assets/Y_Scripts/Area.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Area : MonoBehaviour
+{
+    private int characterCheckPoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Character")
+        {
+            characterCheckPoint++;
+            if (characterCheckPoint >= 4)
+            {
+                //TODO : 囲えた処理を書く
+                Debug.Log("Getだぜ! : " + characterCheckPoint);   
+            }
+        }
+    }
+}
